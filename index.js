356d9c638da7b86cbe3722e665c8a332503ce807
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2020-03-28 11:33:51 +0530

    completed es6

--- index.js
+++ index.js
@@ -504,6 +504,229 @@ const c = new Cat();
 console.log(c.type)
 console.log(c.sound())
 
+const groupByAge = users.reduce((p, c) => {
+  const ageGroup = parseInt(c.age / 10);
+  const key = `${ageGroup}0-${ageGroup}9`;
+  (p[key] = p[key] || []).push(c);
+  return p
+}, {});
+
+console.log(groupByAge)
 
 
+
+const users = [
+  {
+      name: 'yagnesh',
+      gender: 'male',
+      age: 30
+  },
+  {
+      name: 'rohit',
+      gender: 'male',
+      age: 28
+  },
+  {
+      name: 'virat',
+      gender: 'male',
+      age: 25
+  },
+  {
+      name: 'deepika',
+      gender: 'female',
+      age: 32
+  },
+  {
+      name: 'radhika',
+      gender: 'female',
+      age: 26
+  }
+];
+
+
+const newUsers = users.map(user => {
+  if(user.gender === 'male') {
+      return {...user, profession: 'cricketer'}
+  }
+  return {...user, profession: 'actor'}
+});
+
+console.log(newUsers)
+
+class Animal {
+  constructor(type = 'animal') {
+      this.type = type;
+  }
+
+  set type(val) {
+      this._type = val.toUpperCase();
+  }
+
+  get type() {
+      return this._type;
+  }
+
+  sound() {
+      setTimeout(() => {
+          console.log(this.type);
+      }, 1000);
+  }
+}
+
+class Cat extends Animal {
+  constructor(type = 'cat') {
+      super(type)
+  }
+
+  sound(data) {
+      console.log(super.sound())
+      console.log(this.type)
+      console.log(data)
+  }
+}
+
+const a = new Animal();
+console.log(a.sound());
+
+const b = new Cat();
+b.sound('meow...');
+
+// for in
+
+// fo of
+
+
+const obj = { a: 1, b: 2, c: 3};
+
+console.log(Object.keys(obj));
+
+for (const key in obj) {
+    console.log(key,obj[key])
+}
+
+const arr = [1,2,3,4,5,6];
+
+for (const element of arr) {
+    console.log(element);
+}
+
+for (const [key, value] of Object.entries(obj)) {
+    console.log(key)
+    console.log(value)
+}
+
+// setTimeout(callback function, 1000);
+
+console.log(1)
+document.addEventListener('copy', function () {
+    console.log(3)
+})
+console.log(2)
+
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+      resolve('p1 resolve')
+  }, 3000)
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+      reject('p2 reject')
+  }, 2000)
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+      reject('p3 reject')
+  }, 1000)
+});
+
+
+const fetchData = async () => {
+  try {
+      // console.time('p');
+      // const res = await Promise.all([p1,p2,p3]);
+      // console.timeEnd('p');
+      console.time('p1');
+      const res1 = await Promise.race([p1,p2,p3]);
+      console.timeEnd('p1');
+      console.log(res1);
+      // console.time('p')
+      // const res = await p1; // 1sec
+      // console.log(res);
+      // const res1 = await p2; // 2 sec
+      // console.log(res1);
+      // const res2 = await p3; // 3 sec
+      // console.log(res2);
+      // console.timeEnd('p')// 6 sec
+  } catch (error) {
+      console.log(error)
+  }
+};
+
+fetchData();
+
+
+1
+2
+3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// callback
+
+// promise
+
+// generator
+
+// const test = async () => {
+//     await 
+
+// }
+
+
+
+function* add() {
+  yield 1;
+  yield 2;
+  yield 3;
+  return 'hello'
+};
+
+const gen = add();
+
+// console.log(gen.next())
+// console.log(gen.next())
+// console.log(gen.next())
+// console.log(gen.next())
+
+for (const iterator of gen) {
+  console.log(iterator)
+}
+
+
+// function curring
+const calc = (a, b) => {
+  return (func) => {
+      return func(a, b);
+  };
+};
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const multi = (a, b) => a * b;
+
+console.log(calc(1,2)(multi))
 
