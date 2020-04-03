3728987b4dbd3a6a0beddedc9d528e69740391b3
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2020-04-03 20:29:38 +0530

    Commite redux setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { registerObserver } from 'react-perf-devtool';
+import { Provider } from 'react-redux';
 import App from './app';
 import ThemeContext from './contex/ThemeContext';
 import LanguageContext from './contex/LocaleContext';
+import store from './configureStore';
 // import Todos from './Todos';
 import './main.css';
 
@@ -17,10 +19,12 @@ function callback() {}
 registerObserver(options, callback);
 
 ReactDOM.render(
-  <LanguageContext>
-    <ThemeContext>
-      <App />
-    </ThemeContext>
-  </LanguageContext>,
+  <Provider store={store}>
+    <LanguageContext>
+      <ThemeContext>
+        <App />
+      </ThemeContext>
+    </LanguageContext>
+  </Provider>,
   document.getElementById('root'),
 );
--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,15 +1,32 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
 
-const Index = (props) => {
-  console.warn(props);
-  return (
-    <div>
-      <h1>Settings Page</h1>
-    </div>
-  );
-};
+const Index = ({ locale, theme, changeLocale, changeTheme }) => (
+  <div>
+    <h1>Settings Page</h1>
+    <h3>{`Locale: ${locale}`}</h3>
+    <h3>{`Theme: ${theme}`}</h3>
+    <button type="button" onClick={() => changeLocale(locale === 'en' ? 'es' : 'en')}>
+      Change Locale
+    </button>
+    <button type="button" onClick={() => changeTheme(theme === 'light' ? 'dark' : 'light')}>
+      Change Theme
+    </button>
+  </div>
+);
 
 Index.propTypes = {};
 
-export default Index;
+const mapStoreToProps = (state) => ({
+  locale: state.locale,
+  theme: state.theme,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeLocale: (payload) => dispatch({ type: 'CHANGE_LOCALE', payload }),
+  changeTheme: (payload) => dispatch({ type: 'CHANGE_THEME', payload }),
+});
+
+export default connect(mapStoreToProps, mapDispatchToProps)(Index);
